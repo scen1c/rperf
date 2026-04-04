9e73707b11e05d803a03434696152adf78e6d7d0
scen1c <pavel.volovoi@gmail.com>
2026-04-05 00:45:54 +0800

    Add: added to execute rust projects

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 use std::env;
-
+use std::path::Path;
+use std::process::Command;
+use std::process::exit;
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    
-    if args.is_empty() {
-        println!("Usage: rperf <text>");
+    let mut args = env::args().skip(1);
+    let Some(project_path) = args.next() else {
+        eprintln!("Usage: rperf <project-path> [cargo args...]");
+        eprintln!("Try rperf --help for more information.");
+        return;
+    };
+
+    let cargo_args: Vec<String> = args.collect();
+
+    if !Path::new(&project_path).join("Cargo.toml").exists() {
+        eprintln!("Error: Cargo.toml doesnt exist in {}", &project_path);
+        exit(1);
     }
 
-    let x =format!("{}", args.join(" "));
-    println!("{x}")
+    let stat = Command::new("cargo")
+        .arg("run")
+        .args(&cargo_args)
+        .current_dir(&project_path)
+        .status();
+
+    match stat {
+        Ok(stat) => {
+            if stat.success() {
+                println!("Successfully finished with status: {}", stat);
+            } else {
+                eprintln!("Cargo run failed with status: {}", stat);
+                exit(stat.code().unwrap_or(1));
+            }
+        },
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            exit(1);
+        }
+    }
 }
